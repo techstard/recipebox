436f4076093c8c4f097cb66659d7c34a6b6573e0
scumbkt19 <scumbkt19@3245f4a9-1d41-0410-b414-63b167053b95>
2008-03-23 22:00:23 +0000

    renamed Parser to coincide with Nutch standards

---
+++ Web Parser/RecipeboxParser.java
@@ -0,0 +1,295 @@
+//Chris Stafford
+//3/21/2008
+package org.apache.nutch.parse.recipebox;
+
+// JDK imports
+import java.util.Enumeration;
+import java.util.Properties;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.parse.HTMLMetaTags;
+import org.apache.nutch.parse.Parse;
+import org.apache.nutch.parse.HtmlParseFilter;
+import org.apache.nutch.protocol.Content;
+
+// Commons imports
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+// W3C imports
+import org.w3c.dom.DocumentFragment;
+// I/O imports
+import java.io.*;
+
+public class RecipeboxParser implements HtmlParseFilter {
+
+    private Configuration conf;
+
+    public static void main(String args[]) {
+        FileInputStream in = null;
+        int c = 0;
+        int start = 0;
+        int end = 0;
+        char first = ' ';
+        char second = ' ';
+        String page = "";
+        String test = "";
+        String ingredient = "";
+        boolean ingredients = false;
+
+        try {
+            in = new FileInputStream("Web Page.rtf");
+            DataInputStream foo = new DataInputStream(in);
+
+            while (foo.available() != 0) {
+                test = foo.readLine();
+
+                page = page + test;
+
+
+            }
+            foo.close();
+            in.close();
+
+        } catch (Exception E) {
+        }
+
+        //locate the start of the ingredients block
+        start = page.indexOf("h2");
+        page = page.substring(start);
+        start = page.indexOf("h2");
+        end = page.indexOf("</ul");
+        page = page.substring(start, end);
+
+
+        while (true) {
+            start = page.indexOf("<li>");
+            if (start >= 0) {
+                page = page.substring(start + 4);
+                end = page.indexOf("</li>");
+                ingredient = page.substring(0, end);
+                System.out.println(ingredient);
+                //Locate the numerical portion of each ingredient
+                int x = 0;
+                int lastnum = 0;
+                String numeric = "";
+                while (x < ingredient.length()) {
+                    if ((ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                        lastnum = x;
+                    }
+                    x++;
+                }
+                lastnum++;
+                numeric = ingredient.substring(0, lastnum);
+                x = 0;
+                FileOutputStream out;
+                PrintStream p;
+                double numvalue = 0.0;
+                double base = 0.0;
+                double multiple = 0.0;
+                double top = 0.0;
+                double bottom = 0.0;
+                Boolean mult = false;
+                Boolean mixedfraction = false;
+                Boolean fraction = false;
+
+                try {
+                    out = new FileOutputStream("output.txt", true);
+                    p = new PrintStream(out, true);
+
+
+
+
+                    while (x < numeric.length()) {
+                        if ((!mult) && (!mixedfraction) && (!fraction)) {
+                            if ((ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                                numvalue = numvalue * 10;
+                                numvalue = numvalue + (ingredient.charAt(x) - 48);
+                            } else if (ingredient.charAt(x) == 40) {
+                                mult = true;
+                                base = numvalue;
+                                System.out.println("Found a multiplication!");
+                            } else if (ingredient.charAt(x) == 32) {
+                                mixedfraction = true;
+                                base = numvalue;
+                                System.out.println("Found a mixed fraction!");
+                            } else if (ingredient.charAt(x) == 47) {
+                                fraction = true;
+                                top = numvalue;
+                                System.out.println("Found a / sign");
+                            }
+                        } else if ((mult) && (ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                            multiple = multiple * 10;
+                            multiple = multiple + (ingredient.charAt(x) - 48);
+                        } else if ((mixedfraction)) {
+                            if (ingredient.charAt(x) == 47) {
+                                System.out.println("Found the division in the mixed fraction");
+                                fraction = true;
+                            } else if (fraction) {
+                                bottom = bottom * 10;
+                                bottom = bottom + (ingredient.charAt(x) - 48);
+                            } else {
+                                top = top * 10;
+                                top = top + (ingredient.charAt(x) - 48);
+                            }
+
+
+                        } else if ((fraction) && (ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                            bottom = bottom * 10;
+                            bottom = bottom + (ingredient.charAt(x) - 48);
+                        }
+                        x++;
+                    }
+
+                    if (mult) {
+                        numvalue = base * multiple;
+                    } else if (mixedfraction) {
+                        numvalue = (base) + (top / bottom);
+                    } else if (fraction) {
+                        numvalue = top / bottom;
+                    }
+
+                    //print to a file
+                    p.println(ingredient);
+                    p.println(numvalue);
+                    p.close();
+                    out.close();
+                } catch (Exception e) {
+                    System.out.println("Unable to open file!");
+                }
+
+
+            } else {
+                break;
+            }
+        }
+
+    }
+
+    public Parse filter(Content con, Parse p, HTMLMetaTags m, DocumentFragment df) {
+        FileInputStream in = null;
+        int c = 0;
+        int start = 0;
+        int end = 0;
+        char first = ' ';
+        char second = ' ';
+        String page = "";
+        String test = "";
+        String ingredient = "";
+        boolean ingredients = false;
+
+        page = con.getContent().toString();
+        //locate the start of the ingredients block
+        start = page.indexOf("h2");
+        page = page.substring(start);
+        start = page.indexOf("h2");
+        end = page.indexOf("</ul");
+        page = page.substring(start, end);
+
+        while (true) {
+            start = page.indexOf("<li>");
+            if (start >= 0) {
+                page = page.substring(start + 4);
+                end = page.indexOf("</li>");
+                ingredient = page.substring(0, end);
+                System.out.println(ingredient);
+                //Locate the numerical portion of each ingredient
+                int x = 0;
+                int lastnum = 0;
+                String numeric = "";
+                while (x < ingredient.length()) {
+                    if ((ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                        lastnum = x;
+                    }
+                    x++;
+                }
+                lastnum++;
+                numeric = ingredient.substring(0, lastnum);
+                x = 0;
+                FileOutputStream out;
+                PrintStream ps;
+                double numvalue = 0.0;
+                double base = 0.0;
+                double multiple = 0.0;
+                double top = 0.0;
+                double bottom = 0.0;
+                Boolean mult = false;
+                Boolean mixedfraction = false;
+                Boolean fraction = false;
+
+                try {
+                    out = new FileOutputStream("output.txt", true);
+                    ps = new PrintStream(out, true);
+
+                    while (x < numeric.length()) {
+                        if ((!mult) && (!mixedfraction) && (!fraction)) {
+                            if ((ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                                numvalue = numvalue * 10;
+                                numvalue = numvalue + (ingredient.charAt(x) - 48);
+                            } else if (ingredient.charAt(x) == 40) {
+                                mult = true;
+                                base = numvalue;
+                                System.out.println("Found a multiplication!");
+                            } else if (ingredient.charAt(x) == 32) {
+                                mixedfraction = true;
+                                base = numvalue;
+                                System.out.println("Found a mixed fraction!");
+                            } else if (ingredient.charAt(x) == 47) {
+                                fraction = true;
+                                top = numvalue;
+                                System.out.println("Found a / sign");
+                            }
+                        } else if ((mult) && (ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                            multiple = multiple * 10;
+                            multiple = multiple + (ingredient.charAt(x) - 48);
+                        } else if ((mixedfraction)) {
+                            if (ingredient.charAt(x) == 47) {
+                                System.out.println("Found the division in the mixed fraction");
+                                fraction = true;
+                            } else if (fraction) {
+                                bottom = bottom * 10;
+                                bottom = bottom + (ingredient.charAt(x) - 48);
+                            } else {
+                                top = top * 10;
+                                top = top + (ingredient.charAt(x) - 48);
+                            }
+
+
+                        } else if ((fraction) && (ingredient.charAt(x) <= 57) && (ingredient.charAt(x) >= 48)) {
+                            bottom = bottom * 10;
+                            bottom = bottom + (ingredient.charAt(x) - 48);
+                        }
+                        x++;
+                    }
+
+                    if (mult) {
+                        numvalue = base * multiple;
+                    } else if (mixedfraction) {
+                        numvalue = (base) + (top / bottom);
+                    } else if (fraction) {
+                        numvalue = top / bottom;
+                    }
+
+                    //print to a file
+                    ps.println(ingredient);
+                    ps.println(numvalue);
+                    ps.close();
+                    out.close();
+                } catch (Exception e) {
+                    System.out.println("Unable to open file!");
+                }
+            } else {
+                break;
+            }
+        }
+        return p;
+    }
+
+    public void setConf(Configuration conf) {
+        this.conf = conf;
+    }
+
+    public Configuration getConf() {
+        return this.conf;
+    }
+}
+
