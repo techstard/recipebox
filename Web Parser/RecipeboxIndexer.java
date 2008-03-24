1209f8c360175bef86d3efaa21c033c289358fe5
scumbkt19 <scumbkt19@3245f4a9-1d41-0410-b414-63b167053b95>
2008-03-24 01:56:42 +0000

    Added skeletons of Indexer and QueryFilter to facilitate further development

---
+++ Web Parser/RecipeboxIndexer.java
@@ -0,0 +1,58 @@
+package org.apache.nutch.parse.recipebox;
+
+// JDK import
+import java.util.logging.Logger;
+
+// Commons imports
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+// Nutch imports
+import org.apache.nutch.util.LogUtil;
+import org.apache.nutch.fetcher.FetcherOutput;
+import org.apache.nutch.indexer.IndexingFilter;
+import org.apache.nutch.indexer.IndexingException;
+import org.apache.nutch.parse.Parse;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.nutch.crawl.CrawlDatum;
+import org.apache.nutch.crawl.Inlinks;
+
+// Lucene imports
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Document;
+
+public class RecipeboxIndexer implements IndexingFilter {
+    
+  public static final Log LOG = LogFactory.getLog(RecipeboxIndexer.class.getName());
+  
+  private Configuration conf;
+  
+  public RecipeboxIndexer() {
+  }
+
+  public Document filter(Document doc, Parse parse, Text url, 
+    CrawlDatum datum, Inlinks inlinks)
+    throws IndexingException {
+    /* data flow: pull all the fields out of the parse object as in:
+            String recommendation = parse.getData().getMeta("Recommended");
+     * store them in doc objects as Store.YES and Index.UN_TOKENIZED as in:
+     *             Field recommendedField = 
+     *           new Field("recommended", recommendation, 
+     *              Field.Store.YES, Field.Index.UN_TOKENIZED);
+     *       recommendedField.setBoost(5.0f);
+     */       
+
+    return doc;
+  }
+  
+  public void setConf(Configuration conf) {
+    this.conf = conf;
+  }
+
+  public Configuration getConf() {
+    return this.conf;
+  }  
+}
