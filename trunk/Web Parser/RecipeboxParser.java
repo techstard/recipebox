8bbddb449625e7c9bccc83f9d1962d06841334ac
scumbkt19 <scumbkt19@3245f4a9-1d41-0410-b414-63b167053b95>
2008-04-30 00:15:31 +0000

    Final version (???)

--- trunk/Web Parser/RecipeboxParser.java
+++ trunk/Web Parser/RecipeboxParser.java
@@ -14,6 +14,7 @@ import org.w3c.dom.DocumentFragment;
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.HashMap;
 
 import java.text.DecimalFormat;
 
@@ -22,15 +23,13 @@ public class RecipeboxParser implements HtmlParseFilter {
     private Configuration conf;
     private String ingredientString;
     private String amtString;
+    private Double sumTotal;
 
     public Parse filter(Content con, Parse p, HTMLMetaTags m, DocumentFragment df) {
         ingredientString = "";
         amtString = "";
+        sumTotal = 0.0;
         try {
-
-            FileOutputStream out = new FileOutputStream("C:\\NutchOutput.txt");
-            PrintStream ps = new PrintStream(out, true);
-            ps.print("Beginning Function on page: " + con.getUrl());
             int start = 0;
             int end = 0;
 
@@ -50,9 +49,6 @@ public class RecipeboxParser implements HtmlParseFilter {
              * TITLE PARSING
              */
             start = page.toLowerCase().indexOf("<title");
-            if (start < 0) {
-                ps.println("No title for the webpage");
-            }
 
             substring = page.substring(page.indexOf(">",start)+1);
             end = substring.toLowerCase().indexOf("</title>");
@@ -69,10 +65,8 @@ public class RecipeboxParser implements HtmlParseFilter {
             /*
              * START INGREDIENT PARSING
              */
-            ps.println("beginning ingredient parse");
             //trim the HTML to just the body of the text
-            UnitConversion uc = new UnitConversion();
-            System.out.println("Test: "+uc.toCommonUnit(1.0,""));
+            //System.out.println("Test: "+uc.toCommonUnit(1.0,""));
             
             page = page.toLowerCase();
             
@@ -85,34 +79,142 @@ public class RecipeboxParser implements HtmlParseFilter {
             //remove all the tag information
             String temp = removetags(page);
 
-            //locate the Ingredients
-
+            //locate the line breaks which might signify ingredients
             String[] lines = null;
             char lf = 10;
+
             if (temp.split("\n").length > 0) {
-                lines = temp.split("\n");
+                lines = temp.split("[\\s]*\n");
             } else {
-                lines = temp.split(Character.toString(lf));
+                lines = temp.split("[\\s]*" + Character.toString(lf));
             }
+            for (int i = 0; i < lines.length; i++) {
+                lines[i] = lines[i].trim();
+                if(lines[i].matches(".*[\\S]*\r[\\s]*[\\S]+.*"))
+                    lines[i] = lines[i].replace("\r", "");
+            }
+            
+            int numIngredients = 0;
+            HashMap<Integer,String> ingredients = new HashMap<Integer,String>();
 
             for (int i = 0; i < lines.length; i++) {
-                String tempLine = lines[i].trim();
-                if (tempLine.equals("")) {
+                start = 0;
+                end = 0;
+                String tempLine = lines[i];
+                if (tempLine.length() == 0) {
                     continue;
                 }
                 if (Character.isDigit(tempLine.charAt(0))) {
                     if (tempLine.contains(" ")) {
                         for (int j = 0; j < tempLine.length(); j++) {
                             if (Character.isLetter(tempLine.charAt(j))) {
-                                p = addIngredToParse(tempLine, p);
+                                ingredients.put(numIngredients, lines[i]);
+                                numIngredients++;
                                 break;
                             }
                         }
                     }
-                } else if (i > 0 && i < (lines.length - 1) &&
-                        !lines[i - 1].equals("") && Character.isDigit(lines[i - 1].charAt(0)) &&
-                        !lines[i + 1].equals("") && Character.isDigit(lines[i + 1].charAt(0))) {
-                    p = addIngredToParse(lines[i], p);
+                } //catch an ingredient in between two ingredients
+                else if (i > 0 && i < (lines.length - 1) &&
+                        !(lines[i - 1].length() == 0) && Character.isDigit(lines[i - 1].charAt(0)) &&
+                        !(lines[i + 1].length() == 0) && Character.isDigit(lines[i + 1].charAt(0))) {
+                    if (i > 0) {
+                        if (numIngredients > 0) {
+                            start = page.indexOf(lines[i], end);
+                        }
+                    }
+                    ingredients.put(numIngredients, lines[i]);
+                    numIngredients++;
+                } //catch ingredients nestled two-deep between ingredients
+                else if (i > 1 && i < (lines.length - 1) &&
+                        !(lines[i - 2].length() == 0) && Character.isDigit(lines[i - 2].charAt(0)) &&
+                        !(lines[i + 1].length() == 0) && Character.isDigit(lines[i + 1].charAt(0))) {
+
+                    ingredients.put(numIngredients, lines[i]);
+                    numIngredients++;
+                } //catch ingredients located two-deep within ingredients
+                else if (i > 0 && i < (lines.length - 2) &&
+                        !(lines[i - 1].length() == 0) && Character.isDigit(lines[i - 1].charAt(0)) &&
+                        !(lines[i + 2].length() == 0) && Character.isDigit(lines[i + 2].charAt(0))) {
+                    ingredients.put(numIngredients, lines[i]);
+                    numIngredients++;
+                }
+            }
+            if(numIngredients == 0) return p; //No possible ingredient lines found
+            
+            int lastingredient = 0;
+            int firstingredient = -1;
+
+            boolean[] status = new boolean[numIngredients];
+            int[] position = new int[numIngredients];
+            int[] distance = new int[numIngredients];
+            
+            for (int i = 0; i < numIngredients; i++) {
+                if (is_ingredient(ingredients.get(i))) {
+                    status[i] = true;
+                    if (firstingredient < 0) {
+                        firstingredient = i;
+                    }
+                    lastingredient = i;
+                } else {
+                    status[i] = false;
+                }
+            }
+            if(firstingredient == -1 || lastingredient == 0)return p; //No ingredients found
+
+
+            for (int i = firstingredient; i <= lastingredient; i++) {
+                //Verify the status of a possible ingredient line
+                if (isnotingredient(ingredients.get(i))) {
+                    status[i] = false;
+                }
+                else status[i] = true;
+                
+                //Set the position measurement
+                start = 0;
+                if (i == firstingredient) {
+                    temp = ingredients.get(i);
+                    start = page.indexOf(temp);
+                    while (start < 0) {
+                        temp = temp.substring(0, temp.length() - 2);
+                        start = page.indexOf(temp);
+                    }
+                    position[i] = start;
+                } else {
+                    temp = ingredients.get(i);
+                    start = page.indexOf(temp);
+                    while (start < 0) {
+                        temp = temp.substring(0, temp.length() - 2);
+                        start = page.indexOf(temp, position[i - 1]);
+                    }
+                    position[i] = start;
+                }
+            }
+
+            start = -1;
+            end = -1;
+            //set the distance measurement
+            for (int i = firstingredient; i < lastingredient; i++) {
+                if (status[i]) {
+                    start = end;
+                    end = i;
+                    if (start >= 0) {
+                        distance[start] = position[end] - position[start] - ingredients.get(start).length();
+                    }
+                }
+            }
+            for (int i = firstingredient; i < numIngredients; i++) {
+                if (status[i]) {
+                    //ingrediengts tend to be short(er)              
+                    if (ingredients.get(i).length() < 100) {
+                        addIngredToParse(ingredients.get(i),p);
+                    }
+
+                    //check to see if there's any extraneous text showing
+                    //up from the bottom of the HTML
+                    if (distance[i] > 10000) {
+                        break;
+                    }
                 }
             }
 
@@ -122,12 +224,12 @@ public class RecipeboxParser implements HtmlParseFilter {
             p.getData().getContentMeta().set("ingredient", ingredientString);
             //semicolon delimited value string (index corresponds to ingredient)
             p.getData().getContentMeta().set("values", amtString);
+            p.getData().getContentMeta().set("totVolume", Double.toString(sumTotal));
 
             /*
              * END INGREDIENT PARSE
              */
-            ps.close();
-            out.close();
+
             return p;
         } catch (Exception e) {
             System.out.println("CAUGHT EXCEPTION: ");
@@ -149,13 +251,13 @@ public class RecipeboxParser implements HtmlParseFilter {
     }
 
     private Parse addIngredToParse(String ingredient, Parse p) {
-        System.out.println(ingredient);
+        System.out.println("\t'"+ingredient+"'");
         DecimalFormat fourPlaces = new DecimalFormat("0.####");
 
         Pattern pattern = Pattern.compile("(\\d+){1}(( (\\d+)){0,1}(/(\\d+)){0,1})");
         Matcher matcher = pattern.matcher(ingredient);
 
-        matcher.find();
+        if(!matcher.find()) return p;
         String wholeNumStr = matcher.group(1);
         String numeratorStr = matcher.group(4);
         String denominatorStr = matcher.group(6);
@@ -169,7 +271,7 @@ public class RecipeboxParser implements HtmlParseFilter {
             return p;
         } else {
             value = Integer.parseInt(wholeNumStr) +
-                    ((Integer.parseInt(numeratorStr) / 1.0) / (Integer.parseInt(denominatorStr) / 1.0));
+                    ((Integer.parseInt(numeratorStr) + 0.0) / (Integer.parseInt(denominatorStr) + 0.0));
         }
 
         ingredient = ingredient.substring(matcher.end() + 1);
@@ -185,20 +287,13 @@ public class RecipeboxParser implements HtmlParseFilter {
             }
         }
         //Find unit
-        if (ingredient.indexOf(" ", 1) > 0) {
-            //missing this block means the unit is implicit
-            String realUnit = ingredient.substring(0, ingredient.indexOf(" ", 1)).trim();
-            //System.out.printf("realUnit %s\n", realUnit);
-        }
-        //Rest
+        UnitConversion uc = new UnitConversion();
+        value = uc.toCommonUnit(value, ingredient);
         ingredient = ingredient.substring(ingredient.indexOf(" ") + 1);
-        //System.out.printf("ingredient %s\n", ingredient);
 
-
-        //p.getData().getContentMeta().set(ingredient, value.toString());
-        //p.getData().getContentMeta().set("ingredient", ingredient);
         ingredientString += ingredient + ";";
         amtString += fourPlaces.format(value) + ";";
+        sumTotal += value;
         return p;
     }
     private String removetags(String page) {
@@ -242,16 +337,93 @@ public class RecipeboxParser implements HtmlParseFilter {
             var = title.split("-");
 
         for(int i=0;i<var.length;i++) {
-            System.out.println(var[i]);
             if(var[i].trim().matches("([Rr]ecipe(s)?)"));          //Single word "recipe(s)" token
             else if(url.contains(var[i].replace(" ", "").toLowerCase()));     //token (w/out spaces) is in hostname
             else {
-                if(var[i].contains("by")) var[i] = var[i].substring(0,var[i].indexOf("by"));    //Removes author info
-                if(var[i].contains("at")) var[i] = var[i].substring(0,var[i].indexOf("at"));    //Removes site info
+                if(var[i].contains(" by ")) var[i] = var[i].substring(0,var[i].indexOf("by"));    //Removes author info
+                if(var[i].contains(" at ")) var[i] = var[i].substring(0,var[i].indexOf("at"));    //Removes site info
                 return var[i].trim();   //This is the title (Empirically)
             }
         }
         return "";
+    }
+    static boolean is_ingredient(String line) {
+        boolean result = false;
+        result = (result || (line.indexOf("cup") >= 1));
+        result = (result || (line.indexOf("teasp") >= 1));
+        result = (result || (line.indexOf("tablesp") >= 1));
+        result = (result || (line.indexOf(" t ") >= 1));
+        result = (result || (line.indexOf("pound") >= 1));
+        result = (result || (line.indexOf("lbs") >= 1));
+        result = (result || (line.indexOf("lb") >= 1));
+        result = (result || (line.indexOf("pint") >= 1));
+        result = (result || (line.indexOf("quart") >= 1));
+        result = (result || (line.indexOf("qt") >= 1));
+        result = (result || (line.indexOf("halve") >= 1));
+        result = (result || (line.indexOf("pinch") >= 1));
+        result = (result || (line.indexOf("to taste") >= 1));
+        result = (result || (line.indexOf("tsp") >= 1));
+        result = (result || (line.indexOf("tbsp") >= 1));
+        result = (result || (line.indexOf("diced") >= 1));
+        result = (result || (line.indexOf("minced") >= 1));
+        result = (result || (line.indexOf("shredded") >= 1));
+        result = (result || (line.indexOf("thawed") >= 1));
+        result = (result || (line.indexOf("warmed") >= 1));
+        result = (result || (line.indexOf("oz") >= 1));
+        result = (result || (line.indexOf("chopped") >= 1));
+        result = (result || (line.indexOf("sliced") >= 1));
+        result = (result || (line.indexOf("peeled") >= 1));
+        result = (result || (line.indexOf("mashed") >= 1));
+        result = (result || (line.indexOf(" c.") >= 1));
+        result = (result || (line.indexOf(" c ") >= 1));
+        result = (result || (line.indexOf(" tb ") >= 1));
+        result = (result || (line.indexOf(" ts ") >= 1));
+        result = (result || (line.indexOf("pkg") >= 1));
+        result = (result || (line.indexOf("box") >= 1));
+        result = (result || (line.indexOf("packs") >= 1));
+        result = (result || (line.indexOf("ounce") >= 1));
+        result = (result || (line.indexOf("slices") >= 1));
+        result = (result || (line.indexOf("gallon") >= 1));
+        result = (result || (line.indexOf("dash") >= 1));
+        result = (result || (line.indexOf("can") >= 1));
+        result = (result || (line.indexOf("small") >= 1));
+        result = (result || (line.indexOf("medium") >= 1));
+        result = (result || (line.indexOf("large") >= 1));
+        result = (result || (line.indexOf(" sm") >= 1));
+        result = (result || (line.indexOf(" med") >= 1));
+        result = (result || (line.indexOf(" lg") >= 1));
+        result = (result || (line.indexOf(" g ") >= 1));
+
+        if (line.indexOf("dudes") >= 1) {
+            return false;
+        }
+
+        if (line.indexOf("minute meals") >= 1) {
+            return false;
+        }
+
+        return result;
+    }
+    static boolean isnotingredient(String line) {
+        boolean result = false;
+        result = (result || (line.indexOf("minute") >= 1));
+        result = (result || (line.indexOf("yield") >= 1));
+        result = (result || (line.indexOf("time") >= 0));
+        result = (result || (line.indexOf("review") >= 0));
+        result = (result || (line.indexOf(" cook ") >= 0));
+        result = (result || (line.indexOf("until") >= 0));
+        result = (result || (line.indexOf("combine") >= 0));
+        result = (result || (line.indexOf(".jpg") >= 0));
+        result = (result || (line.indexOf("combine") >= 0));
+        result = (result || (line.indexOf("working") >= 0));
+        result = (result || (line.indexOf("preparation") >= 0));
+
+
+        if (line.indexOf(",") == 0) {
+            return true;
+        }
+
+        return result;
     }
 }
 
